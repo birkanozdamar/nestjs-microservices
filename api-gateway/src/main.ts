419ba15c645567427b0c9cd6a78b415fe9a25d23
Birkan Özdamar <birkanozdamar@Birkans-MacBook-Pro-2.local>
2025-01-14 19:38:14 +0300

    Api Gateway Helmet has been added

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
   const options = new DocumentBuilder()
     .setTitle('Microservices Documents')
     .setDescription(' This Document Created for Midset Institute')
