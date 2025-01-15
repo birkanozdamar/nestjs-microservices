04833fe6757f0416bbc818ad5bd64f48463f9580
Birkan Özdamar <birkanozdamar@Birkans-MacBook-Pro-2.local>
2025-01-15 09:47:25 +0300

    Customer Service CRUD files have been added

--- customer-service/src/app.module.ts
+++ customer-service/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 // import { MongooseModule } from '@nestjs/mongoose';
 // import { TypeOrmModule } from '@nestjs/typeorm';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [],
+  imports: [CustomerModule],
   controllers: [AppController],
   providers: [AppService],
 })
